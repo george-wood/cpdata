cd431f18f2214a4f923069c37da78920b65f638c
George Wood <george.wood@nyu.edu>
2022-06-01 17:34:58 -0400

    Initial commit

---
+++ R/tidy.R
@@ -0,0 +1,510 @@
+#' Preprocess arrest data
+#'
+#' @import data.table
+#'
+#' @param file_report File name in working directory or path to .csv file
+#' containing primary arrest data, e.g. date, time, civilian attributes
+#' @param file_officer File name in working directory or path to .csv file
+#' containing corresponding officer data, e.g. appointed date
+#'
+#' @return A data.table containing joined arrest data
+#' @export
+#'
+#' @examples
+#'
+tidy_arrest <- function(file_report, file_officer) {
+
+  a <- fread(
+    file         = file_report,
+    header       = TRUE,
+    showProgress = FALSE,
+    na.strings   = "",
+    select       = toupper(
+      c("cb",
+        "rd",
+        "arrest date/time",
+        "role",
+        "po first name",
+        "po last name",
+        "arrestee race",
+        "arrestee gender",
+        "arrestee age",
+        "statute",
+        "charge type")
+    ),
+    col.names    = c(
+      "eid",
+      "rd",
+      "dt",
+      "role",
+      "first_name",
+      "last_name",
+      "civilian_race",
+      "civilian_gender",
+      "civilian_age",
+      "statute",
+      "charge_type"
+    )
+  )
+
+  a[, dt := as.POSIXct(dt, format = "%d-%b-%Y %H:%M", tz = "UTC")]
+
+  b <- fread(
+    file         = file_officer,
+    header       = TRUE,
+    showProgress = FALSE,
+    na.strings   = "",
+    col.names    = c(
+      "eid",
+      "dt",
+      "role",
+      "first_name",
+      "last_name",
+      "appointed",
+      "birth"
+    )
+  )
+
+  b[, dt := as.POSIXct(dt, format = "%d-%b-%Y %H:%M", tz = "UTC")]
+
+  join_on <- c("eid", "dt", "first_name", "last_name", "role")
+  d <- unique(b)[unique(a), on = join_on]
+
+  message("Finished preprocessing: ", file_report, "& ", file_officer)
+  return(d)
+
+}
+
+
+#' Preprocess assignment data
+#'
+#' @import data.table
+#'
+#' @param file File name in working directory or path to .csv file containing
+#' assignment and attendance data
+#'
+#' @return A data.table containing assignment data
+#' @export
+#'
+#' @examples
+#'
+tidy_assignment <- function(file) {
+
+  d <- fread(
+    file         = file,
+    header       = TRUE,
+    showProgress = FALSE,
+    na.strings   = "",
+    col.names    = c(
+      "date",
+      "unit",
+      "watch",
+      "beat",
+      "car",
+      "start",
+      "end",
+      "last_name",
+      "first_name",
+      "initial",
+      "rank",
+      "star",
+      "gender",
+      "race",
+      "birth",
+      "appointed",
+      "present_for_duty",
+      "absence_code",
+      "absence",
+      "modified_by_last",
+      "modified_by_first",
+      "modified_date"
+    ),
+    key          = c(
+      "last_name",
+      "first_name",
+      "appointed",
+      "birth",
+      "date",
+      "start"
+    )
+  )
+
+  d <- d[
+    d[, .I[which.max(modified_date)], by = eval(key(d))]$V1
+  ]
+
+  d[, oid := .GRP, by = setdiff(key(d), c("date", "start"))]
+
+  d <-
+    d[,
+      .(
+        oid,
+        dt_start = as.POSIXct(
+          x      = paste(date, sprintf("%04s", start)),
+          format = "%d-%b-%Y %H%M",
+          tz     = "UTC"
+        ),
+        dt_end   = as.POSIXct(
+          x      = paste(date, sprintf("%04s", end)),
+          format = "%d-%b-%Y %H%M",
+          tz     = "UTC"
+        ),
+        unit,
+        watch,
+        beat,
+        car,
+        last_name,
+        first_name,
+        initial,
+        rank,
+        star,
+        gender,
+        race,
+        birth,
+        appointed,
+        present_for_duty,
+        absence_code,
+        absence
+      )
+    ]
+
+  d <- d[!is.na(dt_start) & !is.na(dt_end)]
+
+  message("Finished preprocessing: ", file)
+  return(d)
+
+}
+
+
+#' Preprocess contact card data
+#'
+#' @import data.table
+#'
+#' @param file File name in working directory or path to .csv file containing
+#' contact card data
+#'
+#' @return A data.table containing contact card data
+#' @export
+#'
+#' @examples
+#'
+tidy_contact <- function(file) {
+
+  d <- fread(
+    file,
+    header       = TRUE,
+    showProgress = FALSE,
+    na.strings   = "",
+    select       = c(
+      "Contact Date",
+      "DATE",
+      "Time of Stop",
+      "TIME",
+      "FirstPOLN",
+      "1st P.O. LAST NAME",
+      "FirstPOFN",
+      "1st P.O. FIRST NAME",
+      "FirstPOAge (on date of stop)",
+      "1st P.O. AGE",
+      "SecPOLN",
+      "2nd P.O. LAST NAME",
+      "SecPOFN",
+      "2nd P.O.FIRST NAME",
+      "SecPOAge (on date of stop)",
+      "2nd P.O. AGE",
+      "SubRace",
+      "SUBJECT RACE",
+      "SubSex",
+      "SUBJECT SEX",
+      "Contact Type",
+      "CONTACT TYPE DESCRIPTION"
+    ),
+    col.names = c(
+      "date",
+      "time",
+      "first.last_name",
+      "first.first_name",
+      "first.age",
+      "second.last_name",
+      "second.first_name",
+      "second.age",
+      "race",
+      "sex",
+      "type"
+    )
+  )
+
+  d <-
+    dcast(
+      melt(
+        data = unique(d)[, eid := 1:.N],
+        measure.vars = measure(officer, feature, sep = "."),
+        variable.factor = FALSE
+      ),
+      formula = ... ~ feature,
+      value.var = "value"
+    )
+
+  d <-
+    d[!is.na(dt),
+      .(
+        eid,
+        dt = as.POSIXct(x = paste(date, sprintf("%04s", time)),
+                        format = "%d-%b-%y %H:%M",
+                        tz = "UTC"),
+        officer,
+        last_name,
+        first_name,
+        age,
+        race,
+        sex,
+        type
+      )
+    ]
+
+  message("Finished preprocessing: ", file)
+  return(d)
+
+}
+
+
+#' Preprocess use of force data
+#'
+#' @import data.table
+#'
+#' @param file_report File name in working directory or path to .csv file
+#' containing primary tactical response report data, e.g. date, time, officer
+#' @param file_action File name in working directory or path to .csv file
+#' containing corresponding action data, e.g. escort holds
+#'
+#' @return A data.table containing joined tactical response report data
+#' @export
+#'
+#' @examples
+#'
+tidy_force <- function(file_report, file_action) {
+
+  a <- fread(
+    file_report,
+    header       = TRUE,
+    showProgress = FALSE,
+    na.strings   = "",
+    select       = toupper(
+      c("trr_report_id",
+        "dte",
+        "tmemil",
+        "datetime",
+        "dutystatus",
+        "polast",
+        "pofirst",
+        "appointed_date",
+        "subject_injured",
+        "subrace",
+        "subgndr")
+    ),
+    col.names    = tolower
+  )
+
+  if (any(grepl(pattern = "datetime", colnames(d)))) {
+    a[, dt := as.POSIXct(datetime,
+                         format = "%Y-%b-%d %H%M",
+                         tz = "UTC")][]
+  } else {
+    a[, dt := as.POSIXct(paste(dte, sprintf("%04s", tmemil)),
+                         format = "%Y-%m-%d %H%M",
+                         tz = "UTC")][]
+  }
+
+  a <-
+    a[,
+      .(
+        eid              = trr_report_id,
+        dt,
+        on_duty          = dutystatus,
+        last_name        = polast,
+        first_name       = pofirst,
+        appointed        = appointed_date,
+        civilian_injured = subject_injured,
+        civilian_race    = subrace,
+        civilian_gender  = subgndr
+      )
+    ][]
+
+  b <- fread(
+    file_action,
+    header       = TRUE,
+    showProgress = FALSE,
+    na.strings   = "",
+    select       = toupper(
+      c("trr_report_id",
+        "person",
+        "action")
+    ),
+    col.names    = tolower
+  )
+
+  b <- b[person == "Member Action"][
+    ,
+    .(
+      eid = trr_report_id,
+      action
+    )
+  ]
+
+  d <- unique(b[a, on = "eid"])
+
+  message("Finished preprocessing: ", file_report, "& ", file_action)
+  return(d)
+
+}
+
+
+#' Preprocess investigatory stop report data
+#'
+#' @import data.table
+#'
+#' @param file File name in working directory or path to .csv file containing
+#' investigatory stop report data
+#'
+#' @return A data.table containing investigatory stop report data
+#' @export
+#'
+#' @examples
+#'
+tidy_isr <- function(file) {
+
+  d <- fread(
+    file,
+    header       = TRUE,
+    showProgress = FALSE,
+    na.strings   = "",
+    select       = toupper(
+      c("contact_card_id",
+        "contact_date_time",
+        "fo_last",
+        "fo_first",
+        "fo_appointed_dt",
+        "fo_birth_yr",
+        "so_last",
+        "so_first",
+        "so_appointed_date",
+        "so_birth_yr",
+        "race",
+        "sex_code_cd",
+        "enforcement_action_taken_i",
+        "other_reasonable_suspicion_i",
+        "suspect_narcotic_activity_i",
+        "suspicious_object_i",
+        "pat_down_i",
+        "search_i",
+        "vehicle_involved_i")
+    ),
+    col.names    = c(
+      "eid",
+      "dt",
+      "first.last_name",
+      "first.first_name",
+      "first.appointed",
+      "first.birth",
+      "second.last_name",
+      "second.first_name",
+      "second.appointed",
+      "second.birth",
+      "race",
+      "gender",
+      "enforcement",
+      "other_reasonable_suspicion",
+      "suspect_narcotic_activity",
+      "suspicious_object",
+      "pat_down",
+      "search",
+      "vehicle_involved"
+    )
+  )
+
+  d <-
+    dcast(
+      melt(
+        data = unique(d),
+        measure.vars = measure(officer, feature, sep = "."),
+        variable.factor = FALSE
+      ),
+      formula = ... ~ feature,
+      value.var = "value"
+    )
+
+  d[, dt := as.POSIXct(x = dt, format = "%d-%b-%Y %H:%M", tz = "UTC")][]
+
+  message("Finished preprocessing: ", file)
+  return(d)
+
+}
+
+
+#' Preprocess ticket data
+#'
+#' @import data.table
+#'
+#' @param file File name in working directory or path to .csv file containing
+#' ticket data
+#'
+#' @return A data.table containing ticket data
+#' @export
+#'
+#' @examples
+#' file <- "parking_tickets.csv"
+#' tidy_ticket(
+#'   system.file("extdata", file, package = "cpdata", mustWork = TRUE)
+#' )
+tidy_ticket <- function(file) {
+
+  d <- fread(
+    file,
+    header       = TRUE,
+    showProgress = FALSE,
+    na.strings   = "",
+    select       = c(
+      "officer",
+      "ticket_number",
+      "issue_date",
+      "violation_code",
+      "violation_description",
+      "unit_description",
+      "unit",
+      "fine_level1_amount",
+      "fine_level2_amount",
+      "current_amount_due",
+      "total_payments",
+      "ticket_queue",
+      "ticket_queue_date",
+      "hearing_disposition",
+      "geocoded_lng",
+      "geocoded_lat",
+      "geocode_accuracy"
+    ),
+    col.names    = c(
+      "star",
+      "ticket",
+      "dt",
+      "violation_code",
+      "violation",
+      "department",
+      "unit",
+      "fine_1",
+      "fine_2",
+      "current_amount_due",
+      "total_payments",
+      "queue",
+      "queue_date",
+      "hearing_disposition",
+      "longitude",
+      "latitude",
+      "accuracy"
+    )
+  )
+
+  message("Finished preprocessing: ", file)
+  return(d)
+
+}
+
---
+++ README.md
@@ -0,0 +1,59 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# cpdata
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+The functions in cpdata are customized preprocessing steps for specific
+datasets released by the Chicago Police Department.
+
+## Installation
+
+<!-- To install cpdata: -->
+<!-- ```{r install, include = FALSE} -->
+<!-- install.packages("devtools") -->
+<!-- devtools::install_github("george-wood/cpdata") -->
+<!-- ``` -->
+
+## Example
+
+Preprocess ticket data:
+
+``` r
+library(cpdata)
+file <- "parking_tickets.csv"
+tidy_ticket(
+  system.file("extdata", file, package = "cpdata", mustWork = TRUE)
+)
+#> Finished preprocessing: /private/var/folders/qw/kjq09mn10l5cmw355f10qr2h0000gn/T/RtmpM1OQSG/temp_libpath869a7c3840e/cpdata/extdata/parking_tickets.csv
+#>     star     ticket                  dt violation_code
+#>    <int>      <i64>              <POSc>         <char>
+#> 1: 12511   35125343 2001-03-02 18:50:00       0964100C
+#> 2: 15985   32807228 2000-09-21 21:45:00       0964190A
+#> 3:   178 9061102579 1999-10-08 07:57:00       0964150B
+#> 4: 15985   32807221 2000-09-21 21:18:00       0964190A
+#> 5:  3777   33939512 2000-09-21 21:20:00       0964150B
+#>                                   violation department  unit fine_1 fine_2
+#>                                      <char>     <char> <int>  <int>  <int>
+#> 1:     BLOCK ACCESS/ALLEY/DRIVEWAY/FIRELANE        CPD    16     50    100
+#> 2: EXP. METER NON-CENTRAL BUSINESS DISTRICT        CPD    24     30     60
+#> 3:      PARKING/STANDING PROHIBITED ANYTIME        DOF   498     30     60
+#> 4: EXP. METER NON-CENTRAL BUSINESS DISTRICT        CPD    24     30     60
+#> 5:      PARKING/STANDING PROHIBITED ANYTIME        CPD    18     50    100
+#>    current_amount_due total_payments     queue queue_date hearing_disposition
+#>                 <int>          <int>    <char>     <IDat>              <char>
+#> 1:                  0             50      Paid 2001-03-09                    
+#> 2:                 30              0    Define 2000-09-28                    
+#> 3:                  0             30      Paid 2003-01-10                    
+#> 4:                 30              0    Define 2000-09-28                    
+#> 5:                  0             50 Dismissed 2001-10-26          Not Liable
+#>     longitude  latitude accuracy
+#>        <char>    <char>   <char>
+#> 1: -87.692567 41.997949        1
+#> 2: -87.613374 41.865965        1
+#> 3: -87.578981 41.762668        1
+#> 4: -87.613374 41.865965        1
+#> 5:         NA        NA       NA
+```
